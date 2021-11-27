91198a4047bd9c9cca8fa8abafdb0a2f7fa8e0d0
nithinjose-tech <ninjos8@gmail.com>
2021-11-27 18:49:02 +0530

    cors added

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user.routes");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 4000;
-
+const host = "0.0.0.0";
 mongoose
   .connect(
     "mongodb+srv://admin:admin@cluster0.f1tw2.mongodb.net/myDemo?retryWrites=true&w=majority",
@@ -20,6 +21,7 @@ mongoose
   .catch((error) => console.log(`${error},could not connect`));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -29,6 +31,6 @@ app.use("/api/users", userRoutes);
 //   res.send({ message: "Hello World" });
 // });
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Node server is listening on port ${port}`);
 });
